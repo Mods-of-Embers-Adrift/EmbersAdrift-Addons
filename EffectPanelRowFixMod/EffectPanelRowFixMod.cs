e491bca49e2170fc2453a5342e93f11d3e64f0ab
MrJambix <131601090+MrJambix@users.noreply.github.com>
2025-06-01 15:57:06 -0700

    V1.0 EffectPanel

---
+++ EffectPanelRowFixMod/EffectPanelRowFixMod.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class EffectPanelRowFixMod : MelonMod
+{
+    private const int MaxIconsPerRow = 10; // Adjust to desired value for your UI
+
+    public override void OnUpdate()
+    {
+        // Find all active EffectIconPanelUI objects in the scene
+        var panels = GameObject.FindObjectsOfType<MonoBehaviour>()
+            .Where(mb => mb.GetType().Name == "EffectIconPanelUI");
+
+        foreach (var panel in panels)
+        {
+            var grid = ((MonoBehaviour)panel).GetComponent<GridLayoutGroup>();
+            if (grid == null) continue;
+
+            // Count only direct child icons of this panel that are active
+            int iconCount = ((MonoBehaviour)panel).GetComponentsInChildren<UnityEngine.UI.Image>(true)
+                .Count(img => img.gameObject.activeSelf && img.transform.parent == ((MonoBehaviour)panel).transform);
+
+            // Always at least 1 row; collapse to 1 when under MaxIconsPerRow
+            int rowCount = (iconCount > MaxIconsPerRow) ? 2 : 1;
+
+            // Only update if values are not already correct
+            if (grid.constraint != GridLayoutGroup.Constraint.FixedRowCount || grid.constraintCount != rowCount)
+            {
+                grid.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+                grid.constraintCount = rowCount;
+            }
+        }
+    }
+}
